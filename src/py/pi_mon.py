4621948830267ced7516b38a146426de159d81d9
Renan Augusto Starke <xtarke@gmail.com>
2018-10-23 19:16:33 -0300

    Added mqtt commands

--- src/py/pi_mon.py
+++ src/py/pi_mon.py
@@ -1,9 +1,19 @@
 import os
 import time
+import threading
 from picamera import PiCamera
 import paho.mqtt.client as mqtt
 
-camera = PiCamera()
+
+cameraOn = False
+
+camera = PiCamera(resolution=(800, 600))
+
+class MyThread(threading.Thread):
+    def run(self):
+        print("{} started!".format(self.getName()))              # "Thread-x started!"
+        time.sleep(1)                                      # Pretend to work for a second
+        print("{} finished!".format(self.getName()))             # "Thread-x finished!"
 
 def on_disconnect(client, userdata, rc):
     if rc != 0:
@@ -14,18 +24,20 @@ def measure_temp():
     return (temp.replace("temp=",""))
 
 def on_message_print(client, userdata, message):
+    global cameraOn
+
     print("%s %s" % (message.topic, message.payload))
-
+    print("renan")
     
-
-
     if (message.payload == b'1'):
         print("on");
+        #camera.start_preview()
+        cameraOn = True        
     else:
         print("off")
-        
-        
-    
+        cameraOn = False
+        #camera.stop_preview()
+            
 
 def main():
 
@@ -36,7 +48,10 @@ def main():
     mqttc.on_disconnect = on_disconnect
     mqttc.on_message = on_message_print
 
-    mqttc.subscribe("camera/on") 
+    mqttc.subscribe("camera/on")
+
+    mythread = MyThread(name = "Camera_Thread")
+                           
 
     try:
         while True:
@@ -45,9 +60,14 @@ def main():
             print(temperature)
             time.sleep(2)
 
+            if (cameraOn == True):
+                mythread.start()
+
     except KeyboardInterrupt:
         print('Ending...')
-
+        mqttc.disconnect()
+        camera.close()
+        
 
 if __name__ == '__main__':
     main()
